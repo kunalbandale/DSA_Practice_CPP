e1826ade0e97e6b46c36ced70a633e3dcd6ee15d
Kunal Bandale <bandalekunal@gmail.com>
2025-05-28 22:37:47 +0530

    Update automorphicNumber.cpp

--- Basic Code/automorphicNumber.cpp
+++ Basic Code/automorphicNumber.cpp
@@ -1,17 +1,27 @@
 #include<iostream>
- using namespace std;
+#include<cmath>  // for pow()
+using namespace std;
 
 int main() {
-  int number;
-  cin >> number;
+    int number;
+    cin >> number;
 
-  int sq = number * number;
+    int sq = number * number;
 
-  int lastD = sq % 10;
-  if(lastD == number){
-    cout << "Automorphic Number" << endl;
-  } else {
-    cout << "Not a Automorphic Number";
-  }
-  return 0;
+    // Count the number of digits in the original number
+    int digits = 0, temp = number;
+    while (temp > 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    int mod = pow(10, digits);
+
+    if (sq % mod == number) {
+        cout << "Automorphic Number" << endl;
+    } else {
+        cout << "Not an Automorphic Number" << endl;
+    }
+
+    return 0;
 }
